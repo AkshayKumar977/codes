90c9d8469643f6d99087ec0d6228e24f6a314984
AbhishekSrivastava <abhi123456shri@gmail.com>
2025-08-06 16:23:48 +0530

    add addition file

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std
+int main()
+{
+   int n1;
+   cout<<"Enter the first number: ";
+   cin>>n1;
+   cout<<"Enter the second number: ";
+   cin>>n2;
+   cout<<"Addition"<<n1+n2;
+}
